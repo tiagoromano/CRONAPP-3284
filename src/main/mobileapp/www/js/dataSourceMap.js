ecea9654be5ce9c4590063b42c524a122b4f594c
Tiago Romanoi <tiago.romano@techne.com.br>
2021-02-12 13:10:33 +0000

    que negocio

---
+++ src/main/mobileapp/www/js/dataSourceMap.js
@@ -0,0 +1,15 @@
+window.dataSourceMap = window.dataSourceMap || [];
+window.dataSourceMap["app.userManager"] = { customId: "userManager", serviceUrl: "api/cronapi/odata/v2/app/userManager/", serviceUrlODATA: "/api/cronapi/odata/v2/app/userManager" };
+window.dataSourceMap["app.userSecurables"] = { customId: "userSecurables", serviceUrl: "api/cronapi/odata/v2/app/userSecurables/", serviceUrlODATA: "/api/cronapi/odata/v2/app/userSecurables" };
+window.dataSourceMap["app.auditlogquery"] = { customId: "auditlogquery", serviceUrl: "api/cronapi/odata/v2/app/auditlogquery/", serviceUrlODATA: "/api/cronapi/odata/v2/app/auditlogquery" };
+window.dataSourceMap["app.userSecurableManager"] = { customId: "userSecurableManager", serviceUrl: "api/cronapi/odata/v2/app/userSecurableManager/", serviceUrlODATA: "/api/cronapi/odata/v2/app/userSecurableManager" };
+window.dataSourceMap["app.roleSecurableManager"] = { customId: "roleSecurableManager", serviceUrl: "api/cronapi/odata/v2/app/roleSecurableManager/", serviceUrlODATA: "/api/cronapi/odata/v2/app/roleSecurableManager" };
+window.dataSourceMap["app.securableViews"] = { customId: "securableViews", serviceUrl: "api/cronapi/odata/v2/app/securableViews/", serviceUrlODATA: "/api/cronapi/odata/v2/app/securableViews" };
+window.dataSourceMap["app.roleSecurables"] = { customId: "roleSecurables", serviceUrl: "api/cronapi/odata/v2/app/roleSecurables/", serviceUrlODATA: "/api/cronapi/odata/v2/app/roleSecurables" };
+window.dataSourceMap["app.userRoleManager"] = { customId: "userRoleManager", serviceUrl: "api/cronapi/odata/v2/app/userRoleManager/", serviceUrlODATA: "/api/cronapi/odata/v2/app/userRoleManager" };
+window.dataSourceMap["app.device"] = { customId: "device", serviceUrl: "api/cronapi/query/1jy88nt41afza/", serviceUrlODATA: "/api/cronapi/odata/v2/app/device" };
+window.dataSourceMap["app.auth"] = { customId: "auth", serviceUrl: "api/cronapi/odata/v2/app/auth/", serviceUrlODATA: "/api/cronapi/odata/v2/app/auth" };
+window.dataSourceMap["app.roles"] = { customId: "roles", serviceUrl: "api/cronapi/odata/v2/app/roles/", serviceUrlODATA: "/api/cronapi/odata/v2/app/roles" };
+window.dataSourceMap["app.roleManager"] = { customId: "roleManager", serviceUrl: "api/cronapi/odata/v2/app/roleManager/", serviceUrlODATA: "/api/cronapi/odata/v2/app/roleManager" };
+window.dataSourceMap["app.securables"] = { customId: "securables", serviceUrl: "api/cronapi/odata/v2/app/securables/", serviceUrlODATA: "/api/cronapi/odata/v2/app/securables" };
+window.dataSourceMap["app.securableManager"] = { customId: "securableManager", serviceUrl: "api/cronapi/odata/v2/app/securableManager/", serviceUrlODATA: "/api/cronapi/odata/v2/app/securableManager" };
